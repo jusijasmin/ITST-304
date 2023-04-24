3a22b8be8c1fc789ee49a68100b5eb8f626c45a0
Jassss <jasminejusi31@gmail.com>
2023-04-24 22:13:57 +0800

    neww

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-var score = 0;
-var time = 30;
-var intervalId;
+// define the colors
+var colors = [	"red",	"blue",	"green",	"yellow",	"purple",	"orange"];
 
-function start()
+// define the ball array
+var balls = [];
+
+// create the balls
+for (var i = 0; i < 30; i++) {
+	var ball = document.createElement("div");
+	ball.className = "ball";
+	ball.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+	ball.onclick = function() {
+		removeBall(this);
+	};
+	document.querySelector(".container").appendChild(ball);
+	balls.push(ball);
+}
+
+// remove a ball when clicked
+function removeBall(ball) {
+	ball.parentNode.removeChild(ball);
+	var index = balls.indexOf(ball);
+	balls.splice(index, 1);
+	if (balls.length === 0) {
+		alert("Game Over!");
+	}
+}
